90688ddf29561b0fbec92063c68bf118fbedc567
gila <gila@kedoo.com>
2021-07-26 11:33:47 +0300

    Create first github package

---
+++ esm/Test.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+export interface TestProps  {
+    isShow: boolean;
+}
+declare type Test = React.FC<SelectProps>;
+declare const Test: Test;
+export default Test;
---
+++ esm/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Test = () => {
+  return (
+    <div>
+      <h1>123</h1>
+      <h2>312</h2>
+    </div>
+  );
+};
+
+export default Test;
